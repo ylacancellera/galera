a944fd166e4637b86f7901ec9ba8861986e3d8b3
Nirbhay Choubey <nirbhay@mariadb.com>
2016-01-23 00:00:40 -0500

    Fix for an overflow issue.

--- galerautils/src/gu_fdesc.cpp
+++ galerautils/src/gu_fdesc.cpp
@@ -56,7 +56,8 @@ namespace gu
 
         if (0 == err)
         {
-            unsigned long long  const free_size(stat.f_bavail * stat.f_bsize);
+            unsigned long long free_size = stat.f_bavail;
+            free_size *= stat.f_bsize;
 
             if (reserve < free_size)
             {
